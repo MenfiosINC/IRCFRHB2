33c07e0a1a74b4c9b2f276789caf9dc03e545b03
Eduardo Soares <eduardosoares.pt@gmail.com>
2015-01-06 21:38:24 +0000

    Added inline images.

---
+++ example_scripts/images.js
@@ -0,0 +1,13 @@
+setName('images');
+setDescription('displays a image when one is linked');
+
+send('hook_message', 'privmsg');
+this.onMessage = function(e) {
+  propagate(e);
+  var imageRegex = /(https?:\/\/)?.*(jpeg|jpg|png|gif|bmp)/i;
+  var message = e.args[1];
+  var matches = message.match(imageRegex);
+  if (matches) {
+    send(e.context, 'command', 'image', matches[0]);
+  }
+};
