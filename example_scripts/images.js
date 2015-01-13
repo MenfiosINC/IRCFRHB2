4e9e58f3e6772282e30284e43c9f9366cba012ac
Eduardo Soares <eduardosoares.pt@gmail.com>
2015-01-13 21:49:20 +0000

    Remove unnecessary code in the image expansion plugin.

--- example_scripts/images.js
+++ example_scripts/images.js
@@ -2,16 +2,11 @@ setName('images');
 setDescription('displays a image when one is linked');
 
 send('hook_message', 'privmsg');
-send('hook_command', 'toggleimages');
 
 var showImages= true;
 
 this.onMessage = function(e) {
   propagate(e);
-  if (e.type == 'command' && e.name == 'toggleimages'){
-    showImages = !showImages;
-    return;
-  }
   var imageRegex = /(https?:\/\/)?\S*(jpeg|jpg|png|gif|bmp)(\?\S*)?/i;
   var message = e.args[1];
   var matches = message.match(imageRegex);
